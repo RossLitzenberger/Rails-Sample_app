de8b553a3c01eb9f143aed6364b0df79841d22a4
RossLitz <codingclasses.049@gmail.com>
2015-04-29 16:25:08 -0500

    working with Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  # Static_pages Controller
  	root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  # Session Controller
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  # User Controller
   resources :users
 end
