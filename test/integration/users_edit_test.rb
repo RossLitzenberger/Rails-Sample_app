05fc9871e9b7561da87eb74c9c203838d1c80c8f
RossLitz <codingclasses.049@gmail.com>
2015-05-01 19:06:05 -0500

    Authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,18 +5,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		@user = users(:michael)
 	end
 
-	test "unsuccessful edit" do		
-		get edit_user_path(@user)
-		assert_template 'users/edit'
-		patch user_path(@user), user: {name: "",
-																	email: "foo@invalid",
-																	password: 							"foo",
-																	password_confirmation: 	"bar" }
-		assert_template 'users/edit'
-	end
-	  test "successful edit" do
+	test "unsuccessful edit" do	
+    log_in_as(@user)  
     get edit_user_path(@user)
     assert_template 'users/edit'
+    patch user_path(@user), user: {name: "",
+                                  email: "foo@invalid",
+                                  password:               "foo",
+                                  password_confirmation:  "bar" }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    log_in_as(@user)  
+    assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
@@ -30,3 +33,4 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, email
     end
 end
+
