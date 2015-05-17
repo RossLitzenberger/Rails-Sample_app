14c3f43507c69c3d37ada4e1d6bedfd652b332bd
RossLitz <codingclasses.049@gmail.com>
2015-05-16 20:21:40 -0500

    The Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-  
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
   before_save   :downcase_email
   before_create :create_activation_digest
-  has_many :microposts, dependent: :destroy
   
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
@@ -76,6 +80,21 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
   
+  # Follows a user.
+  def follow(other_user)
+   active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a User
+  def unfollow(other_user)
+     active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)   
+  end
+
   private
     
     # Converts email to all lower-case.
