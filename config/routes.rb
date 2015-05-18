43e91106e5a5539b3cfcd759fe004b2fec7eab7d
RossLitz <codingclasses.049@gmail.com>
2015-05-17 23:25:57 -0500

    A web interface for following users

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,15 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-# User Controller
-  resources :users
+# User Controller 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 #  Account activations resource
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
